302ff38c555531cd9d4b554cd8727dd99cfa2031
Craig Cook <boycook@me.com>
2013-09-25 15:13:09 +0100

    Got core service starting

---
+++ test/ServiceSpec.js
@@ -0,0 +1,10 @@
+var should = require('should');
+
+describe('Object', function () {
+    describe('#function', function () {
+        it('should work', function () {
+            var status = true;
+            status.should.be.true;
+        });
+    });
+});
