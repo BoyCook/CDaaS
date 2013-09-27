e4659fa87a91c5ed60a797d25e03db1dadb13f9c
Craig Cook <boycook@me.com>
2013-09-27 13:13:12 +0100

    Using Date.getTime for date diffs

--- public/js/CDaaS.js
+++ public/js/CDaaS.js
@@ -14,7 +14,7 @@ CDaaS.prototype.getAmountFromDate = function(date) {
 };
 
 CDaaS.prototype.getAmountBetweenDates = function(now, then) {
-	return now - then;
+	return now.getTime() - then.getTime();
 };
 
 CDaaS.prototype.parseAmount = function(amount) {
