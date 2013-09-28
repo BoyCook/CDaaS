d23b805bfdd1d992eda34ac14591b497a9b71bd2
Craig Cook <boycook@me.com>
2013-09-28 15:04:09 +0100

    More tests for #getAmountReadable

--- test/CDaaSSpec.js
+++ test/CDaaSSpec.js
@@ -45,66 +45,63 @@ describe('CDaaS', function () {
     });
 
     describe('#getAmountReadable', function () {
-        it('should work for years', function () {
-            var readable = core.getAmountReadable(57361325000);
+        it('should include years', function () {
+            var readable = core.getAmountReadable(1, 298, 21, 42, 5);
             readable.should.eql('1 year 298 days 21 hours 42 minutes 5 seconds');
         });
-        it('should work for days', function () {
-            var readable = core.getAmountReadable(254525000);
+        it('should include days', function () {
+            var readable = core.getAmountReadable(0, 2, 22, 42, 5);
             readable.should.eql('2 days 22 hours 42 minutes 5 seconds');
         });        
-        it('should work for hours', function () {
-            var readable = core.getAmountReadable(9725000);
+        it('should include hours', function () {
+            var readable = core.getAmountReadable(0, 0, 2, 42, 5);
             readable.should.eql('2 hours 42 minutes 5 seconds');
         });                
-        it('should work for minutes', function () {
-            var readable = core.getAmountReadable(125000);
+        it('should include minutes', function () {
+            var readable = core.getAmountReadable(0, 0, 0, 2, 5);
             readable.should.eql('2 minutes 5 seconds');
         });                
-        it('should work for seconds', function () {
-            var readable = core.getAmountReadable(5000);
+        it('should include seconds', function () {
+            var readable = core.getAmountReadable(0, 0, 0, 0, 5);
             readable.should.eql('5 seconds');
         });                
-    });    
-
-    describe('#getAmountReadable', function() {
-        it('should include year when present', function () {
+        it('should just just show year when present', function () {
             var result = core.getAmountReadable(1, 0, 0, 0, 0);
             result.should.eql('1 year');
         });
-        it('should include years when present', function () {
+        it('should just show years when present', function () {
             var result = core.getAmountReadable(2, 0, 0, 0, 0);
             result.should.eql('2 years');
         });        
-        it('should include day when present', function () {
+        it('should just show day when present', function () {
             var result = core.getAmountReadable(0, 1, 0, 0, 0);
             result.should.eql('1 day');
         });
-        it('should include days when present', function () {
+        it('should just show days when present', function () {
             var result = core.getAmountReadable(0, 2, 0, 0, 0);
             result.should.eql('2 days');
         });        
-        it('should include day when present', function () {
+        it('should just show hour when present', function () {
             var result = core.getAmountReadable(0, 0, 1, 0, 0);
             result.should.eql('1 hour');
         });
-        it('should include days when present', function () {
+        it('should just show hours when present', function () {
             var result = core.getAmountReadable(0, 0, 2, 0, 0);
             result.should.eql('2 hours');
         });        
-        it('should include day when present', function () {
+        it('should just show minute when present', function () {
             var result = core.getAmountReadable(0, 0, 0, 1, 0);
             result.should.eql('1 minute');
         });
-        it('should include days when present', function () {
+        it('should just show minutes when present', function () {
             var result = core.getAmountReadable(0, 0, 0, 2, 0);
             result.should.eql('2 minutes');
         });                        
-        it('should include day when present', function () {
+        it('should just show second when present', function () {
             var result = core.getAmountReadable(0, 0, 0, 0, 1);
             result.should.eql('1 second');
         });
-        it('should include days when present', function () {
+        it('should just show seconds when present', function () {
             var result = core.getAmountReadable(0, 0, 0, 0, 2);
             result.should.eql('2 seconds');
         });                
