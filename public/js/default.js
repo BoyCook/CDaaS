7cf385e0f558bd7dad206f264f4b83774d5e0d33
Craig Cook <boycook@me.com>
2013-09-29 18:00:07 +0100

    Tidied client JS

--- public/js/default.js
+++ public/js/default.js
@@ -1,27 +1,35 @@
 var core = undefined;
-var countdown = undefined;
+var data = undefined;
+
+function finished() {
+	$('.amount').text(data.msg);
+	$('.desc').html('');
+}
+
+function setValue(cnt) {
+	var amount = core.parseAmount(cnt);
+	data.remaining = cnt;
+	var text = core.getAmountReadable(amount.years, 
+								         amount.days, 
+								         amount.hours, 
+								         amount.minutes, 
+								         amount.seconds)
+	$('.amount').text(text);	
+}
+
+function gotData(response) {
+	data = response;
+	var params = {
+		target: 0,
+		cnt: data.remaining,
+		finished: finished,
+		setValue: setValue
+	};
+	var countDown = new CountDown(params);
+	countDown.start()
+}
+
 $(document).ready(function () {
 	core = new CDaaS();
-    $.getJSON(document.location.pathname + document.location.search, function(data) {
-    	countdown = data;
-        var params = {
-			target: 0,
-			cnt: data.remaining,
-			finished: function() {
-				$('.amount').text(data.msg);
-			},
-			setValue: function(cnt) {
-				var amount = core.parseAmount(cnt);
-				countdown.remaining = cnt;
-				var text = core.getAmountReadable(amount.years, 
-											         amount.days, 
-											         amount.hours, 
-											         amount.minutes, 
-											         amount.seconds)
-				$('.amount').text(text);
-			}
-        };
-        var countDown = new CountDown(params);
-        countDown.start()
-    });
+    $.getJSON(document.location.pathname + document.location.search, gotData);
 });
