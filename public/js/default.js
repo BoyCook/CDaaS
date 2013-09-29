033824feac9b635204a00b33407c6cf962df94a9
Craig Cook <boycook@me.com>
2013-09-29 14:22:17 +0100

    Using unit for from calculations

--- public/js/default.js
+++ public/js/default.js
@@ -1,11 +1,15 @@
+var core = undefined;
+var countdown = undefined;
 $(document).ready(function () {
-	var core = new CDaaS();
-    $.getJSON(document.location.pathname, function(data) {
+	core = new CDaaS();
+    $.getJSON(document.location.pathname + document.location.search, function(data) {
+    	countdown = data;
         var params = {
 			target: 0,
 			cnt: data.remaining,
 			setValue: function(cnt) {
 				var amount = core.parseAmount(cnt);
+				countdown.remaining = cnt;
 				$('.amount').text(core.getAmountReadable(amount.years, 
 												         amount.days, 
 												         amount.hours, 
