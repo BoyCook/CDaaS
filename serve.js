0a31e466e08c7e4b39992c64a291d8a57c671564
Craig Cook <craig.cook@code.red>
2018-04-03 13:16:41 +0100

    Added APM

--- serve.js
+++ serve.js
@@ -1,2 +1,7 @@
+var apm = require('elastic-apm-node').start({
+    serviceName: 'Count Down as a Service',
+    secretToken: '',
+    serverUrl: 'http://localhost:8200'
+})
 
 require('./lib/server.js').startUp({ port: process.env.PORT || 8080 });
